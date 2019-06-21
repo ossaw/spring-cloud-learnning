a05bcccd93889824416725d00a811be3ad58e099
ossaw <jianghao7625@gmail.com>
2019-06-22 01:18:20 +0800

    提交注册中心配置

--- license-service/src/main/java/com/github/license/LicenseServiceApplication.java
+++ license-service/src/main/java/com/github/license/LicenseServiceApplication.java
@@ -3,11 +3,13 @@ package com.github.license;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 @RefreshScope
-public class LicenseServer {
+public class LicenseServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(LicenseServer.class, args);
+        SpringApplication.run(LicenseServiceApplication.class, args);
     }
 }
